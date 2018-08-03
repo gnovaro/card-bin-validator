2086c59cd735e2dc6ecf27138aa87ece1e43005b
Gustavo Novaro <gnovaro@gmail.com>
2018-08-03 15:50:17 -0300

    Basic bin validation

--- config/bin.php
+++ config/bin.php
@@ -2,6 +2,9 @@
 /**
  * BIN Database Country Argentina - AR
  * @version 1.0.1
+ * FIRST DIGIT: 3 -> AMEX
+ *				4 -> VISA
+ * 				5 -> MASTERCARD
  */
 return array(
 	454659 => array
@@ -13,9 +16,17 @@ return array(
 		'CHAR_LONG' => 16
 	),
 	450979 => array(
-		'BANK' 	    => 'SANTANDER_RIO',
+		'BANK' 	    => 'SANTANDER RIO',
 		'CARD_TYPE' => 'CREDIT',
 		'BRAND'	    => 'VISA',
+		'CVV_LONG'  => 3,
+		'CHAR_LONG' => 16
+	),
+	419551 => array(
+		'BANK' 	    => 'SANTANDER RIO',
+		'CARD_TYPE' => 'CREDIT',
+		'BRAND'	    => 'VISA',
+		'LEVEL'		=> 'GOLD',
 		'CVV_LONG'  => 3,
 		'CHAR_LONG' => 16
 	),
@@ -27,14 +38,54 @@ return array(
 		'CHAR_LONG' => 16
 	),
 	377790 => array(
-		'BANK' 	    => 'SANTANDER_RIO',
+		'BANK' 	    => 'SANTANDER RIO',
 		'CARD_TYPE' => 'CREDIT',
 		'BRAND'	    => 'AMEX',
 		'CVV_LONG'  => 4,
 		'CHAR_LONG' => 15
 	),
+	532384 => array(
+		'BANK' 	    => 'PATAGONIA',
+		'CARD_TYPE' => 'CREDIT',
+		'BRAND'	    => 'MASTERCARD',
+		'LEVEL'		=> 'STANDARD',
+		'CVV_LONG'  => 4,
+		'CHAR_LONG' => 15
+	),
+	430497 => array(
+		'BANK' 	    => 'HIPOTECARIO',
+		'CARD_TYPE' => 'CREDIT',
+		'BRAND'	    => 'VISA',
+		'LEVEL'		=> 'PREMIER',
+		'CVV_LONG'  => 3,
+		'CHAR_LONG' => 16
+	),
+	454074 => array(
+		'BANK' 	    => 'BBVA BANCO FRANCES',
+		'CARD_TYPE' => 'CREDIT',
+		'BRAND'	    => 'VISA',
+		'LEVEL'		=> 'CLASSIC',
+		'CVV_LONG'  => 3,
+		'CHAR_LONG' => 16
+	),
+	558353 => array(
+		'BANK' 	    => 'BBVA BANCO FRANCES',
+		'CARD_TYPE' => 'CREDIT',
+		'BRAND'	    => 'MASTERCARD',
+		'LEVEL'		=> 'BUSINESS',
+		'CVV_LONG'  => 3,
+		'CHAR_LONG' => 16
+	),
+	489321 => array(
+		'BANK' 	    => 'BANCO DE LA PROVINCIA DE BUENOS AIRES',
+		'CARD_TYPE' => 'CREDIT',
+		'BRAND'	    => 'VISA',
+		'LEVEL'		=> 'SIGNATURE',
+		'CVV_LONG'  => 3,
+		'CHAR_LONG' => 16
+	),
 	451766 => array(
-		'BANK' 	    => 'SANTANDER_RIO',
+		'BANK' 	    => 'SANTANDER RIO',
 		'CARD_TYPE' => 'DEBIT',
 		'BRAND'	    => 'VISA',
 		'CVV_LONG'  => 3,
@@ -49,6 +100,13 @@ return array(
 	),
 	501041 => array(
 		'BANK' 	    => 'NACION',
+		'CARD_TYPE' => 'DEBIT',
+		'BRAND'	    => 'MAESTRO',
+		'CVV_LONG'  => 3,
+		'CHAR_LONG' => 16
+	),
+	589562 => array(
+		'BANK' 	    => 'NARANJA',
 		'CARD_TYPE' => 'DEBIT',
 		'BRAND'	    => 'MAESTRO',
 		'CVV_LONG'  => 3,
--- index.php
+++ index.php
@@ -3,22 +3,47 @@
  * Card BIN code validaton API
  * 6 First digit of the card CREDIT/DEBIT
  * @author Gustavo Novaro
- * @version 1.0.0
+ * @version 1.1.0
  * Usage: index.php?bin=377790
  */
 $bins = include('config/bin.php');
 
+$message = array(
+    'code'      => 404,
+    'message'   => 'Request get parameter ?bin={number} is missing'
+);
+
 if(!empty($_GET['bin']))
 {
     $bin = filter_var(trim($_GET['bin']));
-    if(!empty($bins[$bin])){
+
+    if(isset($bins[$bin]))
+    {
         $message = $bins[$bin];
+    } else {
+        $first = substr($bin,0,1);
+        $first = intval($first);
+        switch($first)
+        {
+            case 3:
+                $brand = 'AMEX';
+            break;
+            case 4:
+                $brand = 'VISA';
+            break;
+            case 5:
+                $brand = 'MASTERCARD';
+            break;
+        }
+        if(!empty($brand))
+        {
+            $message = array(
+                'BRAND'	    => $brand,
+                'CVV_LONG'  => ($brand == 'AMEX') ? 4 : 3,
+                'CHAR_LONG' => ($brand == 'AMEX') ? 15 : 16
+            );
+        }
     }
-} else {
-    $message = array(
-        'code'      => 404,
-        'message'   => 'Request get parameter ?bin={number} is missing'
-    );
 }
 header('Content-Type: application/json');
 echo json_encode($message);
