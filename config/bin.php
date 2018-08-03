d9a7ba8d4b557b0947d8692046121a93746aa0d2
Gustavo Novaro <gnovaro@users.noreply.github.com>
2018-08-03 10:15:22 -0300

    AR Basic BIN database

---
+++ config/bin.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * BIN Database Country Argentina - AR
+ * @version 1.0.0
+ */
+return array(
+	454659 => array
+	(
+		'BANK' 	    => 'ICBC',
+		'CARD_TYPE' => 'CREDIT',
+		'TYPE'	    => 'VISA',
+		'CVV_LONG'  => 3,
+		'CHAR_LONG' => 16
+	),
+	450979 => array(
+		'BANK' 	    => 'SANTANDER_RIO',
+		'CARD_TYPE' => 'CREDIT',
+		'TYPE'	    => 'VISA',
+		'CVV_LONG'  => 3,
+		'CHAR_LONG' => 16
+	),
+	525855 => array(
+		'BANK' 	    => 'UALA',
+		'CARD_TYPE' => 'CREDIT',
+		'TYPE'	    => 'MASTERCARTD',
+		'CVV_LONG'  => 3,
+		'CHAR_LONG' => 16
+	),
+	377790 => array(
+		'BANK' 	    => 'SANTANDER_RIO',
+		'CARD_TYPE' => 'CREDIT',
+		'TYPE'	    => 'AMEX',
+		'CVV_LONG'  => 4,
+		'CHAR_LONG' => 15
+	),
+	451766 => array(
+		'BANK' 	    => 'SANTANDER_RIO',
+		'CARD_TYPE' => 'DEBIT',
+		'TYPE'	    => 'VISA',
+		'CVV_LONG'  => 3,
+		'CHAR_LONG' => 16
+	),
+	499859 => array(
+		'BANK' 	    => 'HIPOTECARIO',
+		'CARD_TYPE' => 'DEBIT',
+		'TYPE'	    => 'VISA',
+		'CVV_LONG'  => 3,
+		'CHAR_LONG' => 16
+	),
+);
