af3b37bc5b69fedb9ef23d4aa8e4abb47e349a7d
Gustavo Novaro <gnovaro@gmail.com>
2018-08-03 14:35:44 -0300

    Add maestro

--- config/bin.php
+++ config/bin.php
@@ -1,49 +1,56 @@
 <?php
 /**
  * BIN Database Country Argentina - AR
- * @version 1.0.0
+ * @version 1.0.1
  */
 return array(
 	454659 => array
 	(
 		'BANK' 	    => 'ICBC',
 		'CARD_TYPE' => 'CREDIT',
-		'TYPE'	    => 'VISA',
+		'BRAND'	    => 'VISA',
 		'CVV_LONG'  => 3,
 		'CHAR_LONG' => 16
 	),
 	450979 => array(
 		'BANK' 	    => 'SANTANDER_RIO',
 		'CARD_TYPE' => 'CREDIT',
-		'TYPE'	    => 'VISA',
+		'BRAND'	    => 'VISA',
 		'CVV_LONG'  => 3,
 		'CHAR_LONG' => 16
 	),
 	525855 => array(
 		'BANK' 	    => 'UALA',
 		'CARD_TYPE' => 'CREDIT',
-		'TYPE'	    => 'MASTERCARTD',
+		'BRAND'	    => 'MASTERCARTD',
 		'CVV_LONG'  => 3,
 		'CHAR_LONG' => 16
 	),
 	377790 => array(
 		'BANK' 	    => 'SANTANDER_RIO',
 		'CARD_TYPE' => 'CREDIT',
-		'TYPE'	    => 'AMEX',
+		'BRAND'	    => 'AMEX',
 		'CVV_LONG'  => 4,
 		'CHAR_LONG' => 15
 	),
 	451766 => array(
 		'BANK' 	    => 'SANTANDER_RIO',
 		'CARD_TYPE' => 'DEBIT',
-		'TYPE'	    => 'VISA',
+		'BRAND'	    => 'VISA',
 		'CVV_LONG'  => 3,
 		'CHAR_LONG' => 16
 	),
 	499859 => array(
 		'BANK' 	    => 'HIPOTECARIO',
 		'CARD_TYPE' => 'DEBIT',
-		'TYPE'	    => 'VISA',
+		'BRAND'	    => 'VISA',
+		'CVV_LONG'  => 3,
+		'CHAR_LONG' => 16
+	),
+	501041 => array(
+		'BANK' 	    => 'NACION',
+		'CARD_TYPE' => 'DEBIT',
+		'BRAND'	    => 'MAESTRO',
 		'CVV_LONG'  => 3,
 		'CHAR_LONG' => 16
 	),
